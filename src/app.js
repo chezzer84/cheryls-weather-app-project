5f96dcd3ccdac25cef31c43a7d36272eea54fc24
chezzer84 <115329113+chezzer84@users.noreply.github.com>
2023-10-08 11:26:32 +0800

    Showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
+function displayTemperature(response) {
+  console.log(response.data.temperature.current);
+  console.log(response.data.city);
+  console.log(response.data.condition);
+  console.log(response.data.temperature.humidity);
+  console.log(response.data.wind.speed);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#cityName");
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = response.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = response.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "5t5a295fb1483fe5b29973001ac3oefd";
+let query = "New York";
+let unit = "metric";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=${unit}`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
