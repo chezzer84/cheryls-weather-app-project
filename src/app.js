19d5ca2af248efe2b6cb013051ca7bfac38edfbe
chezzer84 <115329113+chezzer84@users.noreply.github.com>
2023-10-11 21:14:52 +0800

    removed alert

--- src/app.js
+++ src/app.js
@@ -69,9 +69,6 @@ function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
-  if ((cityInputElement.value = "boya")) {
-    alert("Hows the weather looking at my house?");
-  }
 }
 
 function displayFahrentheitTemperature(event) {
